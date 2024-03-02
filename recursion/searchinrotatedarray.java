bc6a9c1d631d4981d6d512a9347022b18491e2fa
Harshit Aggarwal <aggarwalh678@gmail.com>
2024-03-02 19:26:29 +0530

    search roatated sorted array using recursion

---
+++ recursion/searchinrotatedarray.java
@@ -0,0 +1,38 @@
+public class searchinrotatedarray 
+{
+    public static int search(int a[],int tar,int si,int ei)
+    {
+        if(si>ei)
+        return -1;
+        int mid=si+(ei-si)/2;
+        //line 1
+        if(tar==a[mid])
+        {
+            return mid;
+        }
+        if(a[si]<=a[mid])
+        {
+            //left part on line 1
+            if(a[si]<=tar && tar<=a[mid])
+            {
+                return search(a, tar, si, mid-1);
+            }
+            else
+           return search(a, tar, mid+1, ei);
+        }
+        //on line 2
+        else
+        {
+            // line 2 right part
+            if(a[mid]<=tar && tar<=a[ei])
+            return search(a,tar,mid+1,ei);
+            else//complete left part
+            return search(a, tar, si, mid-1);
+        }
+    }
+    public static void main(String args[])
+    {
+        int a[]={4,5,6,7,0,1,2};
+        System.out.println(search(a,2,0,a.length-1));
+    }
+}
